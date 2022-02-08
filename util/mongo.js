36d40b9201bba46199b222a6ffc4b662bc81cec0
carolchep <carolchepsam@gmail.com>
2022-02-08 16:35:11 +0300

    uploading files with cloudinary

--- util/mongo.js
+++ util/mongo.js
@@ -1,13 +1,12 @@
 import mongoose from 'mongoose'
 
-const MONGO_URL = process.env.MONGO_URL
+const MONGODB_URL = process.env.MONGODB_URL
 
-if (!MONGO_URL) {
+if (!MONGODB_URL) {
   throw new Error(
-      'Please define the MONGO_URL environment variable inside .env.local'
+      'Please define the MONGODB_URI environment variable inside .env.local'
   )
 }
-
 
 /**
  * Global is used here to maintain a cached connection across hot reloads
@@ -30,7 +29,7 @@ async function dbConnect() {
       bufferCommands: false,
     }
 
-    cached.promise = mongoose.connect(MONGO_URL, opts).then((mongoose) => {
+    cached.promise = mongoose.connect(MONGODB_URL, opts).then((mongoose) => {
       return mongoose
     })
   }
