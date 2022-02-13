80220f503b8ec61dcfc8200a402871bd50a87686
carolchep <carolchepsam@gmail.com>
2022-02-13 20:36:38 +0300

    http error

--- pages/index.js
+++ pages/index.js
@@ -33,7 +33,7 @@ export const getServerSideProps = async (ctx) => {
         admin = true;
     }
 
-    const res = await axios.get("http://localhost:3000/api/products");
+    const res = await axios.get("https://thethepizzqa-ht5am7lq7-carolchep.vercel.app/api/products");
     return {
         props: {
             pizzaList: res.data,
